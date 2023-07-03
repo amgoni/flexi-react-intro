8735145c4b1f33fae4afab1b5ea8a72bd723649f
Amin Mohammed Goni <amgoni.ug@atbu.edu.ng>
2023-07-03 21:27:19 +0100

    styled-components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
 import Headline from "./Headline";
 import New from "./New";
-import "./Hero.scss";
+import styled from "styled-components";
+// import "./Hero.scss";
 
-export class Hero extends Component {
-  render() {
-    return (
-      <section id="hero">
-        <Headline />
-        <New />
-      </section>
-    );
+const HeroWrapper = styled.section`
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  margin: 0 auto;
+
+  @media (max-width: 768px) {
+    width: 95%;
+    flex-direction: column;
+
+    .headline {
+      padding-right: 0;
+      width: 100%;
+
+      &-writeup {
+        flex-direction: column;
+
+        &-heading {
+          width: 100%;
+          font-size: 1.5rem;
+          margin: 0;
+        }
+
+        &-detail {
+          width: 100%;
+
+          button {
+            margin-top: 1rem;
+          }
+        }
+      }
+    }
   }
-}
+`;
+
+const Hero = () => {
+  return (
+    <HeroWrapper>
+      <Headline />
+      <New />
+    </HeroWrapper>
+  );
+};
 
 export default Hero;
